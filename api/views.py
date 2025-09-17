29a650d5b488edd672837659fd402e7e9bcba695
aref <aref@yahoo.com>
2025-09-17 22:33:01 +0330

    Fixing a little bug and removing an unnecessary part

--- api/views.py
+++ api/views.py
@@ -3,7 +3,6 @@ from .models import Task
 from .serializer import TaskSerializer, UserRegisterSerializer
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-from .custom_permission import MyCustomPermission
 from django.db.models.functions import Lower
 from rest_framework import generics
 
@@ -11,7 +10,7 @@ from rest_framework import generics
 class TaskViewSet(viewsets.ModelViewSet):
     queryset= Task.objects.all()
     serializer_class= TaskSerializer
-    permission_classes= [IsAuthenticated, MyCustomPermission]
+    permission_classes= [IsAuthenticated]
 
     def get_queryset(self):
         return Task.objects.filter(owner= self.request.user)
@@ -26,7 +25,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     ]
     filterset_fields= ['is_done']
     search_fields= ['title', 'description']
-    ordering_fields= ['id', 'lower_title', 'is_done']
+    ordering_fields= ['id', 'title', 'is_done']
 
 
 class RegisterView(generics.CreateAPIView):
